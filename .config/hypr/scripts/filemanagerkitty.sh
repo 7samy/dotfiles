881a819424545aad7d7b3e458351d499db99e29f
7samy <hartmannsamy1@proton.me>
2026-03-29 11:42:13 +0200

    auto: 2026-03-29 11:42

--- .config/hypr/scripts/filemanagerkitty.sh
+++ .config/hypr/scripts/filemanagerkitty.sh
@@ -4,4 +4,4 @@ kitty --class fzfwindows \
       --name fzfwindows \
       --override initial_window_width=900 \
       --override initial_window_height=600 \
-      /home/azu/.config/hypr/scripts/filemanager.sh
+	  yazi
