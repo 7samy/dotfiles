1b74a95589b7750863a2039eeaed73632a3e1dab
7samy <hartmannsamy1@proton.me>
2026-04-10 00:01:16 +0200

    auto: 2026-04-10 00:01

--- .config/hypr/scripts/restart_nvim.sh
+++ .config/hypr/scripts/restart_nvim.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-TARGET="nvim"
-TERMINAL="kitty"
-CLASS_NAME="nvim-reload"
+tmux kill-session -t Nvim 2>/dev/null
 
-PIDS=$(pgrep -f "$TARGET" | grep -v $$)
+pkill -f "[t]itle=tmux_nvim" 2>/dev/null
 
-if [ -n "$PIDS" ]; then
-    kill -9 $PIDS 2>/dev/null
-    sleep 0.3
-fi
-setsid $TERMINAL --class "$CLASS_NAME" -e $TARGET >/dev/null 2>&1 &
+sleep 0.3
 
-sleep 0.1
+setsid nohup kitty --title="tmux_nvim" tmux new-session -s Nvim nvim >/dev/null 2>&1 &
+
+pkill -x "dein_picker" 2>/dev/null
+
 exit 0
