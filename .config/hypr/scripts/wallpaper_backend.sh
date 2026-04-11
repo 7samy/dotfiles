295506fa3ae3d615ca1fb91e30b02235f5413660
7samy <hartmannsamy1@proton.me>
2026-04-11 23:03:49 +0200

    auto: 2026-04-11 23:03

--- .config/hypr/scripts/wallpaper_backend.sh
+++ .config/hypr/scripts/wallpaper_backend.sh
@@ -14,9 +14,11 @@ wal -n -i "$selected_file"
 wpg -s "$selected_file"
 swww img "$selected_file" --transition-type fade --transition-step 10
 
+pkill qs
+sleep 1
+qs &
+
 # ----- FOLGENDE SKRIPTE AUSFÜHREN ----- #
 /home/azu/.config/rofi/scripts/rofi_rgba.sh
-/home/azu/.config/waybar/scripts/waybar_rgba.sh
-/home/azu/.config/hypr/scripts/restart_waybar.sh
 /home/azu/.config/hypr/scripts/restart_nvim.sh
 /home/azu/.config/hypr/scripts/yazi_wal.sh
