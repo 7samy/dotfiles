2a31206f3be97de3ea114fbe0de480ac5fb3b033
7samy <hartmannsamy1@proton.me>
2026-08-20 14:02:23 +0200

    auto: 2026-08-20 14:02

---
+++ .config/hypr/modules/Monitors.lua
@@ -0,0 +1,21 @@
+hl.monitor({
+  output = "DP-2",
+  mode = "2560x1440@240",
+  position = "0x0",
+  scale = 1,
+})
+
+hl.monitor({
+  output = "DP-1",
+  mode = "1920x1080@165",
+  position = "-1920x-100",
+  scale = 1,
+})
+
+hl.monitor({
+  output = "DP-3",
+  mode = "1920x1080@60",
+  position = "2560x-360",
+  scale = 1,
+  transform = 3,
+})
